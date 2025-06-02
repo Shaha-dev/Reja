3f4c3738ad5a17bdc6ee69f7cc4dc2ee8e030526
Bill <shakhzodrasulov98@gmail.com>
2025-06-03 02:48:47 +0900

    feat: develop mit router ejs page

--- server.js
+++ server.js
@@ -1,48 +1,62 @@
-console.log("Web Serverni boshlash");     // Shundan song: npm install express (--save) save yozmasagham by default bizga express save qilip beradi      =     (install qilamiz)
-const express = require("express");        // EXPRESS:  Documentationida undan INSTANT (EXPRESS ning {APP} OBJECTINI yuboradi)
-const app = express();
+// console.log("Web Serverni boshlash");     // Shundan song: npm install express (--save) save yozmasagham by default bizga express save qilip beradi      =     (install qilamiz)
+// const express = require("express");        // EXPRESS:  Documentationida undan INSTANT (EXPRESS ning {APP} OBJECTINI yuboradi)
+// const app = express();
+
+// EXPRES>JS = Package nomi (External package bolip keladi)
+
+// EJS = FAQAT HTML ga OHSHAGAN SYNTAX YARATADI
 
 
 
+// express server yasash uchun
+// express.static ==  css uchun for design
 
-// EXPRESSNING 4 xil shaklda shakillantirish
+// POST: 
+// GET:
 
-// 1 = KIRISH KODI 
-
-// // EXPRESSga kirip kelayotgan malumotlarga bogliq bolgan kodlar yoziladi
-app.use(express.static("public"));   //  Bu har qanday browserdan kirip kelayotkan zaproslar uchun "PUBLIC" folder ochiq degan manoni angladi va koroladi
-                                     //  Google Chrome EXPRESSka request qilayotkan payt "PUBLIC" folderini clientlarga ochip berayapmiz
-                                     //  Keyinchalik bu FOLDER ichiga CSS stylingda boladigan imidjlarni shuni ichiga joylanadi 
-app.use(express.json());    // Bu operationimiz EXPRESS.JSON deganda kirip kelayotkan json formattagi datani OBJECT holatiga ogirip beradi
-app.use(express.urlencoded({extended: true}));    // Bu codimiz HTML dan Traditional request form degan shakl (FORM dan bror nima post qilsak bizni EXPRESS serverimiz qabul qilip oladi. Bu kodni yozish kerak bolmasa oqimidi)   
-const http = require("http");
-
-// // 2 = Sessions Codes 
+// SERVER = Applicationdi ishka tushuradigan narsa
+// WEB SERVER APPLICATION = 
 
 
-// 3 = VIEW codes 
-//BSS  <BackEnd Server SideRendering>   BACKEND da view yasaymiz va biz EJS dan foydalanamiz (EJS ni install qilish kerak)  "npm install ejs"
-app.set("views", "views");            // Bundan keyin CHAP tomonda VIEW FOLDER ochish zarur boladi
-app.set("view engine", "ejs");   
+
+// // EXPRESSNING 4 xil shaklda shakillantirish
+
+// // 1 = KIRISH KODI 
+
+// // // EXPRESSga kirip kelayotgan malumotlarga bogliq bolgan kodlar yoziladi
+// app.use(express.static("public"));   //  Bu har qanday browserdan kirip kelayotkan zaproslar uchun "PUBLIC" folder ochiq degan manoni angladi va koroladi
+//                                      //  Google Chrome EXPRESSka request qilayotkan payt "PUBLIC" folderini clientlarga ochip berayapmiz
+//                                      //  Keyinchalik bu FOLDER ichiga CSS stylingda boladigan imidjlarni shuni ichiga joylanadi 
+// app.use(express.json());    // Bu operationimiz EXPRESS.JSON deganda kirip kelayotkan json formattagi datani OBJECT holatiga ogirip beradi (tushunadigan til)
+// app.use(express.urlencoded({extended: true}));    // Bu codimiz HTML dan Traditional request form degan shakl (FORM dan bror nima post qilsak bizni EXPRESS serverimiz qabul qilip oladi. Bu kodni yozish kerak bolmasa oqimidi)   
+// const http = require("http");
+
+// // // 2 = Sessions Codes 
 
 
-// // 4 Routing code
-
-app.get("/Hello", function(req, res) {  // "/" manashu tayoqdan keyin bror nima yozsa boladi masalan "GIFT" (Masalan asosiy page ichida GIFT bolimiga otsa ishlatiladi)
-res.end(`<h1 style="background: red">Hello MIT 27</h1>`);  // AGAR STYLE qoshmoqchi bolsak SUPPER STRING ichiga yozoshimiz kerak STYLE bolmasa ikta "" shunaqani ichiga yozsak boladi
-});
-
-
-app.get("/gift", function(req, res) {  
-res.end(`<h1>You are in the gift page</h1>`);  
-});
+// // 3 = VIEW codes 
+// //BSSR  <BackEnd Server SideRendering>   BACKEND da view yasaymiz va biz EJS dan foydalanamiz (EJS ni install qilish kerak)  "npm install ejs"
+// app.set("views", "views");            // Bundan keyin CHAP tomonda VIEW FOLDER ochish zarur boladi
+// app.set("view engine", "ejs");   
 
 
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT , function() {
-    console.log(`The server is running successefully on PORT: ${PORT}`);
-});
+// // // 4 Routing code
+
+// app.get("/Hello", function(req, res) {  // "/" manashu tayoqdan keyin bror nima yozsa boladi masalan "GIFT" (Masalan asosiy page ichida GIFT bolimiga otsa ishlatiladi)
+// res.end(`<h1 style="background: red">Hello MIT 27</h1>`);  // AGAR STYLE qoshmoqchi bolsak SUPPER STRING ichiga yozoshimiz kerak STYLE bolmasa ikta "" shunaqani ichiga yozsak boladi
+// });
+
+
+// app.get("/gift", function(req, res) {  
+// res.end(`<h1>You are in the gift page</h1>`);  
+// });
+
+
+// const server = http.createServer(app);
+// let PORT = 3000;
+// server.listen(PORT , function() {
+//     console.log(`The server is running successefully on PORT: ${PORT}`);
+// });
 
 
 
@@ -53,7 +67,7 @@ server.listen(PORT , function() {
 
 // GIT IGNORE bizi GITimizga yozilishi kerak bolmagan narsalarni yozamiz. NODE MODULE ni yozmemiz
 
-// git status  = GIT ni statusini korip beradi va nimaiki kamchilik bolsa chiqarip beradi
+// git status  = GIT ni statusini korip beradi va nimaiki kamchilik bolsa chiqarip beradi VA nima ozgarish sodir bolgan bolsa chiqarip beradi
 
 // git add .   = Shu bilan osha kamchiliklarni TOLDIRADI va yana git status qilsak result korishimiz mumkun
 
@@ -66,6 +80,10 @@ server.listen(PORT , function() {
 // git hub - faqat mani desktopimda emas boshqalar ham ishlata olish uchun ishlatamiz (????)
 
 // npm run dev
+
+// git pull origin master - Ohirgi qilingan ishni download qilip oladi GITHUB dagi
+
+// git reset --hard = Git eski holiga qaytadi
 
 
 
@@ -116,6 +134,99 @@ server.listen(PORT , function() {
 
 
 
+
+
+
+
+// REMOTE PUSH nima degani ??????????????
+
+// git commit --amend (nima degani) ?????????? :wq (write and quit) GIT GRAPH ichiga yozip beradi va tugatadi
+
+
+
+
+
+
+
+// EJS FRAMEWORK
+
+
+// // TEST CODE
+
+// Starting poin
+console.log("Web Serverni boshlash");     
+const express = require("express");        
+const res = require("express/lib/response");
+const app = express();   // FUNCTION
+const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
+
+
+
+
+// 1 EXPRESS SETTINGS
+app.use(express.static("public"));    // PUBLIC FOLDER = Tashqi olamga ochiqlash ichiga CSS HTML kirip ketadi       (OBJECT, MIDDLEWARE) PUBLIC FOLDER OCHIP BERADI
+app.use(express.json());              // COMPUTER TUSHUNADIGON FORMAT                                               (OBJECT, MIDDLEWARE)
+app.use(express.urlencoded({extended: true}));   // MALUMOTLARNI QABUL QILIP OLADI                                  (OBJECT, MIDDLEWARE)
+
+
+// 2 yoq--
+
+// 3 VIEW (INTERFACE)
+app.set("views", "views");            
+app.set("view engine", "ejs"); 
+
+// 4 ROUTING CODLAR   (Qachonki browserdan request kelsa keyin oqiydi)
+
+app.post("/create-item", (req, res) => {
+    // TODO: code with db here
+});
+
+app.get('/mit',(req, res) => {
+    res.render("mit", {user: user});
+});
+
+// const http = require('http');        // (************)  shuni qoymasam bolmadi
+app.get("/", function (req, res) {  //(request & response)  (BULAR IKTA OBJECTLAR)
+    res.render("harid");   // RENDER = REQUESTGA RESPONSE QILISH UCHUN YARATILADI
+});
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT , function() {
+    console.log(`The server is running successefully on PORT: ${PORT}`);
+});
+
+
+
+
+
+
+// ============ RENDER ===============
+
+
+// GET & POST (method)
+
+// GET = SERVERGA JONATILGAN REQUESTGA RESPONSE JONATIP BERADI () request jonatip javob olamiz DATA OLAMIZ
+  
+// POST = SERVERGA DATA HAM KIRITA OLAMIZ REQUESTHAM OLAOLAMIZ
+
+
+
+
+// PATTERNS <=========================================
+
+// 1.   ARCHITECTUAL PATTERN     - MVC, CACHE                                  - LOYIHALASHTIRAMIZ
+// 2.   DESIGN PATTERN           - MIDDLEWARE, DEPENDANCY INJECTION            - KICHIK BOLAKLARGA BEZAK BERAMIZ
 
 
 
--- train.js
+++ train.js
@@ -1,4 +1,64 @@
-// B-TASK
+// B-TASK "EJS FRAMEWORK"
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>PLAN</title>
+//     <link 
+//     rel="stylesheet"
+//    href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+//     integrity="sha384-GJzZqFGwb1QTINGwy59ffF1BuGJpLSa9DkkMp0DgiMDm41YMj70gWKYbI76tMS"
+//     crossorigin="anonymous"
+//     />
+// </head>
+// <body style="background-color:bisque">
+//     <div class="container">
+//         <h1 class="display4 text-center py-1">Reja va Maqsadlar</h1>
+    
+    
+//     </div>
+// </body>
+// </html>
+
+// starting point
+
+// console.log("Web Swerverni boshlash");     
+// const express = require("express");        
+// const app = express();
+
+// // 1
+// app.use(express.static("public"));
+// app.use(express.json());    
+// app.use(express.urlencoded({extended: true})); 
+
+// // 2 yoq--
+
+// // 3
+// app.set("views", "views");            
+// app.set("view engine", "ejs"); 
+
+// // 4
+
+// const http = require('http');        // (************)  shuni qoymasam bolmadi
+// app.get("/", function (req, res) {
+//     res.render("harid");
+// });
+
+// const server = http.createServer(app);
+// let PORT = 3000;
+// server.listen(PORT , function() {
+//     console.log(`The server is running successefully on PORT: ${PORT}`);
+// });
+
+
+
+
+
+
+
+
 
 
 
@@ -10,4 +70,34 @@
 
 // npm run start = HOMEWORK ni ishka tushurmoqchi bolsak shunda ishlatamiz
 
-console.log("TRAIN AREA");
+// console.log("TRAIN AREA");
+
+// TEST CODE
+
+// Starting poin "EXPRESS FRAMEWORK"
+
+// console.log("Web Swerverni boshlash");     
+// const express = require("express");        
+// const app = express();
+
+// // 1
+// app.use(express.static("public"));
+// app.use(express.json());    
+// app.use(express.urlencoded({extended: true})); 
+
+// // 2 yoq--
+
+// // 3
+// app.set("views", "views");            
+// app.set("view engine", "ejs"); 
+
+// // 4
+// app.get("/", function(req, res) {
+// res.end("Hello MIT 27");
+// });
+
+// const server = http.createServer(app);
+// let PORT = 3000;
+// server.listen(PORT , function() {
+//     console.log(`The server is running successefully on PORT: ${PORT}`);
+// });
